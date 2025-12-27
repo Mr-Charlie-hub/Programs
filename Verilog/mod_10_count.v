86aa1ae040f694afa86f51c052520da620a4ccf2
Rohith D <rohithd.et23@rvce.edu.in>
2025-12-28 02:55:52 +0530

    Implement 10-count module in Verilog

---
+++ Verilog/mod_10_count.v
@@ -0,0 +1,13 @@
+module d_c10(
+  input in,clk,rst,
+  output reg[3:0]count);
+  
+  always@(posedge clk)begin
+    if(rst)
+      count<=4'd0;
+    else if(count==4'd9)
+      count<=4'd0;
+    else count<=count+1;
+  end
+endmodule
+  
